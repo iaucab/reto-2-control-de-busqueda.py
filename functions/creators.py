5bd5de42cc497b6415379840aa460e40af90f17b
Jose Manuel Romero <josemanuelromerop@gmail.com>
2019-04-18 09:50:05 -0400

    Fix a bug

--- functions/creators.py
+++ functions/creators.py
@@ -13,7 +13,7 @@ def insertAtPosition(matrix, blockType, str):
   # Validations
   if len(position) != 2:
     print("Error, you should enter 2 numbers. Ex: '26 18'")
-    return insertInPosition(matrix, blockType, 'Try again:')
+    return insertAtPosition(matrix, blockType, 'Try again:')
 
   position[0] = int(position[0])
   position[1] = int(position[1])
@@ -21,11 +21,11 @@ def insertAtPosition(matrix, blockType, str):
   if (position[0] < 0 or position[0] > len(matrix)-1 or
       position[1] < 0 or position[1] > len(matrix[0])-1):
     print("Error, position out of range")
-    return insertInPosition(matrix, blockType, 'Try again:')
+    return insertAtPosition(matrix, blockType, 'Try again:')
 
   if matrix[position[0]][position[1]] != 0:
     print("Error, this cell is not empty")
-    return insertInPosition(matrix, blockType, 'Try again:')
+    return insertAtPosition(matrix, blockType, 'Try again:')
 
   # Insert new cell
   matrix[position[0]][position[1]] = blockType
